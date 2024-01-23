f11ce9185192553e15fe007ef25bb13a0dfb42a9
shahrukh <mohiuddinshahrukh@gmail.com>
2024-01-23 22:34:40 +0100

    fixed some isues

--- assignment4.py
+++ assignment4.py
@@ -68,42 +68,57 @@ def main():
         # Implement the CKY algorithm in model/recognizer.py and use it as a recognizer
         # Provide a list of grammatical and ungrammatical sentences and test the recognizer
 
-        grammatical = ["what is the cheapest ticket from memphis to miami ."]
-        ungrammatical = ["how much does flight number a nineteen cost from new york to los angeles on monday morning ."]
+        grammatical_sentences = ["what is the cheapest ticket from memphis to miami .",
+                                 "please book a one way coach fare from chicago to indianapolis on united flight two ninety two next wednesday .",
+                                 "how much does flight number a nineteen cost from new york to los angeles on monday morning .",
+                                 "what kind of aircraft is american 's flight fifteen oh one that departs at six fifty nine p.m .",
+                                 "how much does first class on that flight cost and how much does coach on that flight cost .",
+                                 "please list the flights leaving newark stopping over in seattle for approximately five hours and then on to san francisco .",
+                                 "i 'd like the cheapest round trip ticket from minneapolis to san diego arriving in san diego before seven p.m .",
+                                 "what is the cheapest first class ticket available flying from new york to miami one way but also round trip .",
+                                 "please give me the number of flights that depart from lester b pearson international airport on april twenty fifth .",
+                                 "what is the cheapest one way flight from phoenix to san diego that arrives in the morning on thursday june second .",
+                                 "i 'd like to fly from indianapolis to houston on t w a and the plane should arrive around eleven a.m ."]
+        ungrammatical_sentences = [
+            "prices .",
+            "what is e w r .",
+            "i 'd like an afternoon flight .",
+            "i 'd like to leave on a saturday .",
+            "list u s air flights to dallas to boston .",
+            "i 'd like to fly out of chicago for less than a hundred fifty dollars round trip .",
+            "show me flights from detroit to san diego on tuesday may third .",
+            "show me flights from chicago to kansas city leaving around seven p.m. thursday .",
+            "i would like to leave on thursday morning may fifth before six a.m .",
+            "i need a first class round trip airfare from detroit to saint petersburg .",
+            "how far is the airport from new york 's la guardia to downtown ."]
 
-        for sents in grammatical:
-            val = recognize(grammar, nltk.word_tokenize(sents))
-            print('\nGrammatical sentence: ')
-            if val:
-                print("{} is in the language of CFG.".format(sents))
-            else:
-                print("{} is not in the language of CFG.".format(sents))
+        print("Testing Grammatical Sentences:")
 
-        for sents in ungrammatical:
-            val = recognize(grammar, nltk.word_tokenize(sents))
-            print('\nUngrammatical sentence: ')
-            if val:
-                print("{} is in the language of CFG.".format(sents))
-            else:
-                print("{} is not in the language of CFG.".format(sents))
+        for sent in grammatical_sentences:
+            result = recognize(grammar, nltk.word_tokenize(sent))
+            print(f"{sent}: {result}")
 
+        print("\nTesting Ungrammatical Sentences:")
+        for sent in ungrammatical_sentences:
+            result = recognize(grammar, nltk.word_tokenize(sent))
+            print(f"{sent}: {result}")
     elif args.parser:
         # Extend CKY recognizer into a parser in model/parser.py
         # Provide the list of ATIS test sentences with tab-separated numbers of parse trees
         # Choose an ATIS test sentence with a number of parses p such that 1 < p < 5 and provide pictures of its parses
 
-        selected_sentence = t[1][0]
-        print("selected_sentence: ", selected_sentence)
-        print("ID\t Predicted_Tree\tLabeled_Tree")
+        chosen_sent = t[1][0]
+        print("Sentence: ", chosen_sent)
+        print("S#\t\t Predicted\t\tLabeled")
         for idx, sents in enumerate(t):
             tree = parse(grammar, sents[0])
             print("{}\t {}\t \t{}".format(idx, len(tree), sents[1]))
 
         # Choose an ATIS test sentence with a number of parses p such that 1 < p < 5
-        selected_tree = parse(grammar, selected_sentence)
+        selected_tree = parse(grammar, chosen_sent)
 
         print("\nSelected Sentence:")
-        print(" ".join(selected_sentence))
+        print(" ".join(chosen_sent))
         print("\nNumber of Parses: {}".format(len(selected_tree)))
         print("\nParse Trees:")
         for tree in selected_tree:
@@ -114,7 +129,7 @@ def main():
         # Compute the number of parse trees for an entry A ∈ Ch(i, k) from the chart with backpointers
         # Use model/parser.py to compute the number of parse trees without actually computing the parse tree
 
-        print("ID\t Predicted_Tree\tLabeled_Tree")
+        print("S#\t\t Predicted\t\tLabeled")
         for idx, sents in enumerate(t):
             num_tree = count(grammar, sents[0])
             print("{}\t {}\t \t{}".format(idx, num_tree, sents[1]))
